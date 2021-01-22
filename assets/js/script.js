eb3c21b63f7d387818fafe310f9fd3ee6c3c5d44
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-01-22 21:34:58 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-const btn = document.getElementById("ham_icon");
+const btn = document.getElementById("menu__mobile__icon");
 btn.addEventListener("click", function () {
   const main__menu = document.getElementById("main__menu");
+  const main__title = document.getElementById("main__title");
   main__menu.style.display = "block";
+  main__title.style.display = "none";
 });
